8344157b8437f81e44ac00ba1d1d03af897e6f91
Darío Pérez <59544296+darionic30@users.noreply.github.com>
2020-09-26 22:29:48 -0300

    Add files via upload

---
+++ Scripts/scripts.js
@@ -0,0 +1,31 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const xmasEve = `25 Dec 2020`;
+
+function countDown() {
+	const xmasEveDate = new Date(xmasEve);
+	const currentDate = new Date();
+
+	const totalSec = Math.floor(xmasEveDate - currentDate) / 1000;
+
+	const days = Math.floor(totalSec / 3600 / 24);
+	const hours = Math.floor((totalSec / 3600) % 24);
+	const minutes = Math.floor((totalSec / 60) % 60);
+	const seconds = Math.floor(totalSec) % 60;
+
+	daysEl.innerHTML = days;
+	hoursEl.innerHTML = formatTime(hours);
+	minutesEl.innerHTML = formatTime(minutes);
+	secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+	return time < 10 ? `0${time}` : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
