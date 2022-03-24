fa1b29519e2996a8d7218e0046d274d7445e3945
dario.nic <dario.nic30@gmail.com>
2022-03-24 17:32:02 -0300

    Current date corrected

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,27 +3,28 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const xmasEve = `25 Dec 2021`;
+const today = new Date();
+const xmasEve = new Date(today.getFullYear(), 11, 25);
 
 function countDown() {
-	const xmasEveDate = new Date(xmasEve);
-	const currentDate = new Date();
+  const xmasEveDate = new Date(xmasEve);
+  const currentDate = new Date();
 
-	const totalSec = Math.floor(xmasEveDate - currentDate) / 1000;
+  const totalSec = Math.floor(xmasEveDate - currentDate) / 1000;
 
-	const days = Math.floor(totalSec / 3600 / 24);
-	const hours = Math.floor((totalSec / 3600) % 24);
-	const minutes = Math.floor((totalSec / 60) % 60);
-	const seconds = Math.floor(totalSec) % 60;
+  const days = Math.floor(totalSec / 3600 / 24);
+  const hours = Math.floor((totalSec / 3600) % 24);
+  const minutes = Math.floor((totalSec / 60) % 60);
+  const seconds = Math.floor(totalSec) % 60;
 
-	daysEl.innerHTML = days;
-	hoursEl.innerHTML = formatTime(hours);
-	minutesEl.innerHTML = formatTime(minutes);
-	secondsEl.innerHTML = formatTime(seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-	return time < 10 ? `0${time}` : time;
+  return time < 10 ? `0${time}` : time;
 }
 
 countDown();
